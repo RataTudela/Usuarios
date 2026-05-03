d94b3aa5c5c832e3e6a2f0f695713953349b3a25
RataTudela <ben.olavarria@duocuc.cl>
2026-05-03 19:34:19 -0400

    modificacion de 1 pequeño error que hacia que chocara con el gateway y no conectara con el frontend

--- src/main/java/com/example/usuarios/controller/UsuarioController.java
+++ src/main/java/com/example/usuarios/controller/UsuarioController.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/usuarios/usuario")
-@CrossOrigin(origins = "*")
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
